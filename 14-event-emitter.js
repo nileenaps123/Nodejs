106e441e2a278ee36a71fec95750d9ef5d1700fb
Nileena <nileena.ps123@gmail.com>
2026-06-29 11:34:27 +0530

    node basics-events,streams

---
+++ 14-event-emitter.js
@@ -0,0 +1,12 @@
+const EventEmitter=require('events');
+
+const customEmitter=new EventEmitter();
+
+customEmitter.on('response',(name,age)=>{
+    console.log(`data received ${name} ${age}`)
+});
+
+customEmitter.on('response',()=>{
+    console.log(`some resp`)
+});
+customEmitter.emit('response','john',34);
